76fa37d0da799c3ba64b5f2c6f9c1f5ba6104ad6
Ian Rufus <ian.j.rufus@gmail.com>
2017-03-23 21:50:20 +0000

    RouletteBet tests
    Stubbed out tests for the RouletteBet class

---
+++ src/test/java/com/ianrufus/betting/RouletteBetTests.java
@@ -0,0 +1,89 @@
+package com.ianrufus.betting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RouletteBetTests {
+	
+	@Test
+	public void getWinningsWithLosingBetReturnsZero() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void getWinningsOnSingleNumberReturnsCorrectAmount() {
+		
+	}
+	
+	@Test
+	public void getWinningsOnLowBetReturnsCorrectAmount() {
+		
+	}
+	
+	@Test
+	public void getWinningsOnHighBetReturnsCorrectAmount() {
+		
+	}
+	
+	@Test
+	public void getWinningsOnRedBetReturnsCorrectAmount() {
+		
+	}
+	
+	@Test
+	public void getWinningsOnBlackBetReturnsCorrectAmount() {
+		
+	}
+	
+	@Test
+	public void getWinningsOnEvenBetReturnsCorrectAmount() {
+		
+	}
+	
+	@Test
+	public void getWinningsOnOddBetReturnsCorrectAmount() {
+		
+	}
+	
+	@Test
+	public void getWinningsOnFirstDozenBetReturnsCorrectAmount() {
+		
+	}
+	
+	@Test
+	public void getWinningsOnSecondDozenBetReturnsCorrectAmount() {
+		
+	}
+	
+	@Test
+	public void getWinningsOnThirdDozenBetReturnsCorrectAmount() {
+		
+	}
+	
+	@Test
+	public void getWinningsOnFirstColumnBetReturnsCorrectAmount() {
+		
+	}
+	
+	@Test
+	public void getWinningsOnSecondColumnBetReturnsCorrectAmount() {
+		
+	}
+	
+	@Test
+	public void getWinningsOnThirdColumnBetReturnsCorrectAmount() {
+		
+	}
+	
+	@Test
+	public void getWinningsTwiceWithWinningBetReturnsZeroSecondAttempt() {
+		
+	}
+	
+	@Test
+	public void betTypeTakesPrecedenceOverNumbers() {
+		
+	}
+
+}
